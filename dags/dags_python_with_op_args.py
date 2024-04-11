fe33989ea78332ae6da30bd8e697cbb2ab91b49b
hyoseok <hyosug.bang@wesang.com>
2024-04-11 12:45:49 +0900

    dag_id 수정

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from common.common_func import regist
 
 with DAG(
-    dag_id="dags_python_task_decorator",
+    dag_id="dags_python_with_op_args",
     schedule="0 2 * * 1",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False,
