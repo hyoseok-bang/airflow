29bc721612316dc85c79c82d5543ba5f9450ba4e
hyoseok <hyosug.bang@wesang.com>
2024-04-11 11:54:09 +0900

    python decorator 실행

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2021,1,1,tz="Asia/False"),
+    catchup=False,
+) as dag:
+    
+    @task(task_id="python_task1")
+    def print_context(some_input):
+        print(some_input)
+
+python_task1 = print_context("Python Task Decorator 실행")  # task_id와 task object명도 되도록 일치시키는게 좋음
