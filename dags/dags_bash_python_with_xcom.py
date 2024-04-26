667d8d19c0948e508ff7f94c6201d4de261c65d4
hyoseok <hyosug.bang@wesang.com>
2024-04-26 23:55:39 +0900

    dags_bash_python_with_xcom.py 수정

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_bash_python_with_xcom",
     schedule="10 0 * * *",
-    schedule_interval=pendulum.datetime(2024, 4, 15, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 4, 15, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
 
@@ -18,9 +18,9 @@ with DAG(
     bash_pull = BashOperator(
         task_id="bash_pull",
         env={
-            "STATUS": "{{ ti.xcom_pull(task_id='python_push)['status'] }}",
-            "DATA": "{{ ti.xcom_pull(task_id='python_push)['data'] }}",
-            "OPTIONS_CNT": "{{ ti.xcom_pull(task_id='python_push)['options_cnt'] }}",
+            "STATUS": "{{ ti.xcom_pull(task_id='python_push)['status'] }}",  # Good
+            "DATA": "{{ ti.xcom_pull(task_id='python_push)['data'] }}",  # [1,2,3]
+            "OPTIONS_CNT": "{{ ti.xcom_pull(task_id='python_push)['options_cnt'] }}",  # 100
         },
         bash_command="echo $STATUS && echo $DATA && echo $OPTION_CNT",
     )
@@ -38,8 +38,8 @@ with DAG(
     @task(task_id="python_pull")
     def python_pull_xcom(**kwargs):
         ti = kwargs["ti"]
-        status_value = ti.xcom_pull(key="bash_pushed")
-        return_value = ti.xcom_pull(task_id="bash_push")
+        status_value = ti.xcom_pull(key="bash_pushed")  # 200
+        return_value = ti.xcom_pull(task_id="bash_push")  # PUSH_COMPLETE
         print("status_value: " + str(status_value))
         print("return_value: " + return_value)
 
