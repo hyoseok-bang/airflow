dc02bf21ace0921270d2fd8ed7f3d79581a270ad
hyoseok <hyosug.bang@wesang.com>
2024-05-16 18:10:10 +0900

    dags_bash_with_variables.py 업로드

---
+++ dags/dags_bash_with_variables.py
@@ -0,0 +1,20 @@
+from airflow.operators.bash import BashOperator
+from airflow.models import Variable
+import pendulum
+
+with DAG(
+    dag_id="dags_bash_with_variables",
+    schdeule="10 9 * * *",
+    start_date=pendulum.datetime(2023, 4, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    # 1. Variable 라이브러리를 이용한 전역변수 호출
+    var_value = Variable.get("sample_key")
+    bash_var_1 = BashOperator(
+        task_id="bash_var_1", bash_command=f"echo variable:{var_value}"
+    )
+    
+    # 2. Jinja template을 이용한 전역변수 호출
+    bash_var_2 = BashOperator(
+        task_id="bash_var_2", bash_command="echo varialbe:{{var.value.sample_key}}"
+    )
