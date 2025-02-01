83c1b32e6549f73d0b7c55b5abeff2e58817d275
Hyoseok-Bang <hyoseok1989@gmail.com>
2025-02-01 20:05:06 +0900

    Update dags_python_with_xcom_eg1.py
    print kwargs

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -17,6 +17,7 @@ with DAG(
 
     @task(task_id="python_xcom_push_task2")
     def xcom_push2(**kwargs):
+        print(kwargs)
         ti = kwargs["ti"]
         ti.xcom_push(key="result1", value="value_2")
         ti.xcom_push(key="result2", value=[1, 2, 3])
